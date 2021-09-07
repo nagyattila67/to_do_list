bf6a568052a67f2078c2718afbe43406d8b96699
Attila <quamdiuetiam@hotmail.com>
2021-09-07 03:19:50 +0200

    20210907

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+export const calcFullTime = function (array) {
+    const fullTime = array.reduce((acc, num) => acc + num, 0);
+    return fullTime;
+}
+
+export const calcRestTime = function (duration, isDone, myActivities) {
+    const restTimeArray = myActivities.map(value => {
+        let myValue = value.duration;
+        if (value.isDone == true) { myValue = 0 };
+        return myValue;
+    })
+    const restTime = restTimeArray.reduce((acc, value) => acc + value, 0)
+    return restTime;
+}
